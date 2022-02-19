4847e752c1af86886ae94561e1ef344183bef62c
Erick <yiperick@hotmail.com>
2022-02-19 12:27:44 -0700

    Updated front end code

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Journal from './components/Journal';
+import background from './img/background.png';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Journal />
   </React.StrictMode>,
   document.getElementById('root')
 );
